4bfb7e01dfe6234007d29bf1f00518ac50bec0a6
Jen Wachter <jwachter@jhu.edu>
2024-12-05 13:13:26 -0500

    Added the ability to view jobs in processed and processing lists. Previously, you could only view pending jobs

--- src/cli/commands/QueueCommands/JobsCommand.php
+++ src/cli/commands/QueueCommands/JobsCommand.php
@@ -4,7 +4,6 @@
 namespace PhpRedisQueue\cli\commands\QueueCommands;
 
 use PhpRedisQueue\cli\commands\Command;
-use PhpRedisQueue\models\Queue;
 use PhpRedisQueue\managers\QueueManager;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
@@ -27,23 +26,47 @@ class JobsCommand extends Command
         'queuename',
         InputArgument::REQUIRED,
         'The name of the queue',
+      )
+      ->addArgument(
+        'type',
+        InputArgument::OPTIONAL,
+        'The type of jobs to return: `pending`, `processed`, or `processing`',
+        'pending'
+      )
+      ->addArgument(
+        'status',
+        InputArgument::OPTIONAL,
+        'If requesting process jobs, optionally filter by status: `success` or `failed`',
+        false,
       );
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     $queueName = $input->getArgument('queuename');
+    $queueType = $input->getArgument('type');
+    $status = $input->getArgument('status');
+
+    // validation
+    $queueType = in_array($queueType, ['pending', 'processing', 'processed']) ? $queueType : 'pending';
+    $status = $status === false || in_array($status, ['failed', 'success']) ? $status : 'success';
+    
     $queue = $this->queueManager->getQueue($queueName);
 
-    $jobs = $queue->getJobs('pending');
+    $jobs = $queue->getJobs($queueType);
+
+    // filter by status, if necessary
+    if ($status) {
+      $jobs = array_filter($jobs, fn ($job) => $job->status === $status);
+    }
 
     if (empty($jobs)) {
       $output->writeln('No jobs found.');
     } else {
       $table = new Table($output);
       $table
-        ->setHeaders(['ID', 'Datetime initialized', 'Job name'])
-        ->setRows(array_map(fn ($job) => [$job->id, $job->datetime, $job->jobName], $jobs));
+        ->setHeaders(['ID', 'Datetime initialized', 'Job name', 'Job status'])
+        ->setRows(array_map(fn ($job) => [$job->id, $job->datetime, $job->jobName, $job->status], $jobs));
 
       $table->render();
     }
